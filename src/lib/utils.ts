d070c51cbcf9be35a3c39452bfb75dd6e8ff6b1b
Nate Houk <nate@epsilonrecords.com>
2024-07-02 12:31:42 +0200

    Refactor

--- src/lib/utils.ts
+++
@@ -1,3 +0,0 @@
-export const serializeNonPOJO = (obj: any) => {
-    return structuredClone(obj);
-};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,25 @@
-<script lang="ts">
-  import { superForm } from 'sveltekit-superforms';
-  import { Button } from 'svelte-ux';
-  import { ThemeSelect, Card } from 'svelte-ux';
-
-  export let data;
-
-  // Client API:
-  const { form } = superForm(data.form);
-
+<script>
+	import { Button } from 'svelte-ux';
 </script>
-<Card title="Title" subheading="Subheading" />
-<!-- <Button>Click here</Button>
-<ThemeSelect lightThemes={["light"]} darkThemes={["dark"]} />
-<div class="card p-4">  
-    Welcome to our exclusive members-only music distribution site, where artists and enthusiasts come together to support and celebrate the world of music. As a collective backed by our devoted members, we provide a host of benefits designed to amplify your musical reach. Membership includes access to exclusive releases, behind-the-scenes content, powerful distribution tools, networking opportunities with industry professionals, and invitations to private events. Join us and become part of a dynamic community that empowers and elevates the art of music distribution.
-</div>
-<form method="POST">
-  <label for="name">Name</label>
-  <input type="text" name="name" bind:value={$form.name} />
 
-  <label for="email">E-mail</label>
-  <input type="email" name="email" bind:value={$form.email} />
-
-  <div><button>Submit</button></div>
-</form> -->
+<main class="p-2 grid place-items-center content-center">
+	<h1 class="text-2xl font-semibold mb-2">Welcome to ✨Svelte UX✨</h1>
+	<div>
+		<Button
+			href="https://svelte-ux.techniq.dev"
+			target="_blank"
+			variant="fill-light"
+			color="primary"
+		>
+			Documentation
+		</Button>
+		<Button
+			href="https://svelte-ux.techniq.dev/customization"
+			target="_blank"
+			variant="fill-light"
+			color="primary"
+		>
+			Customization
+		</Button>
+	</div>
+</main>
