39a73affe17bbd0fe3c11d7e37db4b981fa62bdb
Nate Houk <nate@epsilonrecords.com>
2024-07-03 09:52:23 +0200

    Refactor

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,39 +1,64 @@
 <script lang="ts">
-	import { AppBar, AppLayout, Card, Button, NavItem, Tooltip, ThemeSelect, settings } from 'svelte-ux';
+	import {
+		AppBar,
+		AppLayout,
+		Card,
+		Button,
+		NavItem,
+		Tooltip,
+		ThemeSelect,
+		settings
+	} from 'svelte-ux';
 
 	import { page } from '$app/stores';
 	import '../app.postcss';
 
 	import { getSettings } from 'svelte-ux';
-
-	const { currentTheme, themes } = getSettings();
-
-	// Change theme
-	currentTheme.setTheme('cyberpunk');
-
 </script>
 
 <AppLayout>
 	<svelte:fragment slot="nav">
-		<NavItem
-			path="/"
-			text="Home"
-			icon="M10,20V14H14V20H19V12H22L12,3L2,12H5V20H10Z"
-			currentUrl={$page.url}
-		/>
+		<div class="card p-4 w-4/5">
+			<NavItem
+				path="/"
+				text="Home"
+				icon="<svg class='h-8 w-8 text-gray-900'  width='24' height='24' viewBox='0 0 24 24' stroke-width='2' stroke='currentColor' fill='none' stroke-linecap='round' stroke-linejoin='round'>  <path stroke='none' d='M0 0h24v24H0z'/><polyline points='5 12 3 12 12 3 21 12 19 12' /><path d='M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7' /><path d='M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6' /></svg>"
+				currentUrl={$page.url}
+			/>
 
-		<NavItem
-			path="/about"
-			text="About"
-			icon="M11,9H13V7H11M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M11,17H13V11H11V17Z"
-			currentUrl={$page.url}
-		/>
+			<NavItem
+				path="/about"
+				text="About"
+				icon="<svg class='h-8 w-8 text-gray-900'  width='24' height='24' viewBox='0 0 24 24' stroke-width='2' stroke='currentColor' fill='none' stroke-linecap='round' stroke-linejoin='round'><path stroke='none' d='M0 0h24v24H0z'/><line x1='4' y1='20' x2='7' y2='20' /><line x1='14' y1='20' x2='21' y2='20' /><line x1='6.9' y1='15' x2='13.8' y2='15' />  <line x1='10.2' y1='6.3' x2='16' y2='20' /><polyline points='5 20 11 4 13 4 20 20' /></svg>"
+				currentUrl={$page.url}
+			/>
+			<NavItem
+				path="/settings"
+				text="Settings"
+				icon="<svg class='h-8 w-8 text-gray-900' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><circle cx='12' cy='12' r='3' />  <path d='M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z' /></svg>"
+				currentUrl={$page.url}
+			/>
+			<NavItem
+				path="/artists"
+				text="Artists"
+				icon="<svg class='h-8 w-8 text-gray-900'  fill='none' viewBox='0 0 24 24' stroke='currentColor'><path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z'/></svg>"
+				currentUrl={$page.url}
+			/>
+		</div>
 	</svelte:fragment>
 
 	<AppBar title="Epsilon Distribution">
 		<div slot="actions" class="flex gap-3">
 			<Tooltip title="Change color theme" placement="left" offset={2}>
-				<ThemeSelect lightThemes={["cyberpunk"]} darkThemes={["dark"]} />
+				<ThemeSelect lightThemes={['light']} darkThemes={['dark']} />
+			</Tooltip>
+			<Tooltip title="Discord" placement="left" offset={2}>
+				<Button
+					icon="M20.33 5.06C18.78 4.33 17.12 3.8 15.38 3.5 15.17 3.89 14.92 4.4 14.74 4.82 12.9 4.54 11.07 4.54 9.26 4.82 9.09 4.4 8.83 3.89 8.62 3.5 6.88 3.8 5.21 4.33 3.66 5.06 0.53 9.79-0.32 14.41 0.1 18.96 2.18 20.52 4.19 21.46 6.17 22.08 6.66 21.4 7.1 20.69 7.48 19.93 6.76 19.66 6.07 19.33 5.43 18.94 5.6 18.81 5.77 18.68 5.93 18.54 9.88 20.39 14.17 20.39 18.07 18.54 18.23 18.68 18.4 18.81 18.57 18.94 17.92 19.33 17.24 19.66 16.52 19.94 16.9 20.69 17.33 21.41 17.82 22.08 19.8 21.46 21.82 20.52 23.9 18.96 24.4 13.69 23.05 9.11 20.33 5.06ZM8.01 16.17C6.83 16.17 5.86 15.06 5.86 13.71 5.86 12.36 6.81 11.25 8.01 11.25 9.22 11.25 10.19 12.36 10.17 13.71 10.17 15.06 9.22 16.17 8.01 16.17ZM15.99 16.17C14.8 16.17 13.83 15.06 13.83 13.71 13.83 12.36 14.78 11.25 15.99 11.25 17.19 11.25 18.17 12.36 18.14 13.71 18.14 15.06 17.19 16.17 15.99 16.17Z"
+					href="https://discord.gg/FbgwPM4K"
+					class="p-2"
+					target="_blank"
+				/>
 			</Tooltip>
 			<Tooltip title="View repository" placement="left" offset={2}>
 				<Button
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,7 +13,8 @@
 <main class="p-2 grid place-items-center content-center">
 
 	<Card title="Epsilon Distribution" subheading="A modern music distribution platform." />
-	<div class="card p-4">  
+	<div class="card p-4 w-4/5">  
 		Welcome to our exclusive members-only music distribution site, where artists and enthusiasts come together to support and celebrate the world of music. As a collective backed by our devoted members, we provide a host of benefits designed to amplify your musical reach. Membership includes access to exclusive releases, behind-the-scenes content, powerful distribution tools, networking opportunities with industry professionals, and invitations to private events. Join us and become part of a dynamic community that empowers and elevates the art of music distribution.
 	</div>
+	<Card title="Create Release" subheading="Get started by creating a release." />
 </main>
