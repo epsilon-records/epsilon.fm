92c385122820d6e2c790e380e2ae943e37d20482
Nate Houk <nate@epsilonrecords.com>
2024-07-03 21:43:31 +0200

    Refactor

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,9 +1,11 @@
 import type { LayoutServerLoad } from './$types';
+import { NODE_ENV } from '$env/static/private';
 
 export const load: LayoutServerLoad = async (event) => {
 	const session = await event.locals.auth();
 
 	return {
-		session
+		session,
+		nodeEnv: NODE_ENV
 	};
 };
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import '../app.css';
 	import Header from './Header.svelte';
+	import type { LayoutServerData } from './$types';
+	import { Badge } from '$lib/components/ui/badge/index.js';
+
+	export let data: LayoutServerData;
 </script>
 
 <div class="app">
@@ -12,6 +16,7 @@
 
 	<footer>
 		<p>Made with ❤️ by Epsilon</p>
+		<Badge>{data.nodeEnv}</Badge>
 	</footer>
 </div>
 
