6943b1f88fb120eff6ecebf9294539980bcba6f1
Nate Houk <nate@epsilonrecords.com>
2024-07-04 20:47:47 +0200

    Refactor

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -6,7 +6,7 @@ import { CLERK_SECRET_KEY } from '$env/static/private';
 export const handle: Handle = sequence(
 	handleClerk(CLERK_SECRET_KEY, {
 		debug: true,
-		protectedPaths: ['/admin'],
+		protectedPaths: ['/admin', '/profile'],
 		signInUrl: '/sign-in'
 	})
 );
