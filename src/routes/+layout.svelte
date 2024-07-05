f6af7a6319430a5984462da9239f797be5559b75
Nate Houk <nate@epsilonrecords.com>
2024-07-05 12:11:41 +0200

    Refactor home page.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,7 +19,7 @@
 
 	<footer>
 		<p class="text-sm text-muted-foreground">Made with ❤️ by Epsilon Records</p>
-		<Badge class="m-2" variant={data.nodeEnv == 'development' ? 'destructive' : 'default'}
+		<Badge class="m-2" variant={data.nodeEnv == 'development' ? 'outline' : 'default'}
 			>{data.nodeEnv}</Badge
 		>
 	</footer>
