d6f32ccb2c46772c47b74e22379e69746c14cad5
Nate Houk <nate@epsilonrecords.com>
2024-07-05 12:25:50 +0200

    Refactor FAQ page.

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -11,20 +11,12 @@
 	<Card.Content>
 		<Accordion.Root class="w-full self-center sm:max-w-[100%]">
 			<Accordion.Item value="item-1">
-				<Accordion.Trigger>Is it accessible?</Accordion.Trigger>
-				<Accordion.Content>Yes. It adheres to the WAI-ARIA design pattern.</Accordion.Content>
+				<Accordion.Trigger>Is it free?</Accordion.Trigger>
+				<Accordion.Content>No.</Accordion.Content>
 			</Accordion.Item>
 			<Accordion.Item value="item-2">
-				<Accordion.Trigger>Is it styled?</Accordion.Trigger>
-				<Accordion.Content>
-					Yes. It comes with default styles that matches the other components' aesthetic.
-				</Accordion.Content>
-			</Accordion.Item>
-			<Accordion.Item value="item-3">
-				<Accordion.Trigger>Is it animated?</Accordion.Trigger>
-				<Accordion.Content>
-					Yes. It's animated by default, but you can disable it if you prefer.
-				</Accordion.Content>
+				<Accordion.Trigger>Do you deliver to Spotify?</Accordion.Trigger>
+				<Accordion.Content>Yes.</Accordion.Content>
 			</Accordion.Item>
 		</Accordion.Root>
 	</Card.Content>
