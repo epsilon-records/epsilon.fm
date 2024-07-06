84dc9b7e2fd4141c4dd0a4a1eab93a7d3c3bb07b
Nate Houk <nate@epsilonrecords.com>
2024-07-06 17:19:13 +0200

    Refactor

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,18 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import preprocessReact from 'svelte-preprocess-react/preprocessReact';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [vitePreprocess(), preprocessReact()],
-	onwarn: (warning, handler) => {
-		if (warning.code === 'css-unused-selector') {
-			return;
-		}
-		handler(warning);
-	},
+	preprocess: [vitePreprocess()],
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		csrf: {
-			checkOrigin: false // #TODO: set to true when deploying to production
+			checkOrigin: false // TODO: set to true when deploying to production
 		},
 		alias: {
 			'@/*': './src/lib/*'
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,24 @@
-import { sentrySvelteKit } from "@sentry/sveltekit";
+import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 
 export default defineConfig({
-	plugins: [sentrySvelteKit({
-        sourceMapsUploadOptions: {
-            org: "epsilon-records",
-            project: "epsilon"
-        }
-    }), sveltekit(), enhancedImages()],
+	plugins: [
+		sentrySvelteKit({
+			sourceMapsUploadOptions: {
+				org: 'epsilon-records',
+				project: 'epsilon'
+			}
+		}),
+		sveltekit(),
+		enhancedImages()
+	],
+	build: {
+		commonjsOptions: {
+			transformMixedEsModules: true
+		}
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
