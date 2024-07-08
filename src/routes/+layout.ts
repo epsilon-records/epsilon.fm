37dafd1072f9c471a831cf9d7f8ba74ccabd5e96
Nate Houk <nate@epsilonrecords.com>
2024-07-08 11:31:33 +0200

    Refactor

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -9,8 +9,8 @@ injectSpeedInsights();
 
 export const load = async () => {
 	if (browser) {
-		posthog.init('phc_j5S8Foca22TArMjuKIWR0DhyRt821XV9IqUY1mrOkQJ', {
-			api_host: 'https://us.i.posthog.com',
+		posthog.init('phc_b84fhRZ4SOSxH3M0yXibkJEvNzMOo5D9QUEKjidSQWN', {
+			api_host: 'https://eu.i.posthog.com',
 			person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
 		});
 	}
