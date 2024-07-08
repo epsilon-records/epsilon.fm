38e695103638305dfab42b1bb3f89754415335f4
Nate Houk <nate@epsilonrecords.com>
2024-07-08 13:08:27 +0200

    Refactor

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,13 +3,14 @@ import { inject } from '@vercel/analytics';
 import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 import posthog from 'posthog-js';
 import { browser } from '$app/environment';
+import { env } from '$env/dynamic/private';
 
 inject({ mode: dev ? 'development' : 'production' });
 injectSpeedInsights();
 
 export const load = async () => {
 	if (browser) {
-		posthog.init('phc_b84fhRZ4SOSxH3M0yXibkJEvNzMOo5D9QUEKjidSQWN', {
+		posthog.init(env.POSTHOG_KEY, {
 			api_host: 'https://eu.i.posthog.com',
 			person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
 		});
