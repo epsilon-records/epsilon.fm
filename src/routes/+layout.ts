f09a0b8ea4cb0b53a0c0fd8710c16ba3e50cffd4
Nate Houk <nate@epsilonrecords.com>
2024-07-08 11:03:43 +0200

    Add PostHog analytics.

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,18 @@
 import { dev } from '$app/environment';
 import { inject } from '@vercel/analytics';
 import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+import posthog from 'posthog-js';
+import { browser } from '$app/environment';
 
 inject({ mode: dev ? 'development' : 'production' });
 injectSpeedInsights();
+
+export const load = async () => {
+	if (browser) {
+		posthog.init('phc_j5S8Foca22TArMjuKIWR0DhyRt821XV9IqUY1mrOkQJ', {
+			api_host: 'https://us.i.posthog.com',
+			person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
+		});
+	}
+	return;
+};
