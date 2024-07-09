6bdf285b79064ed126ff4a1e4192260e22b61ab3
Nate Houk <nate@epsilonrecords.com>
2024-07-09 07:27:20 +0200

    Refactor

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -10,7 +10,7 @@ injectSpeedInsights();
 
 export const load = async () => {
 	if (browser) {
-		posthog.init(PUBLIC_POSTHOG_KEY, {
+		posthog.init(PUBLIC_POSTHOG_API_KEY, {
 			api_host: 'https://eu.i.posthog.com',
 			person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
 		});
