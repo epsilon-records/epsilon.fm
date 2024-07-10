bf3c3e896c2cce43bfaa1f521099b1fc8c4b7dcc
Nate Houk <nate@epsilonrecords.com>
2024-07-10 21:58:55 +0200

    Refactor

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -1,10 +1,8 @@
 import type { LayoutServerLoad } from './$types';
 import { NODE_ENV } from '$env/static/private';
 
-export const load: LayoutServerLoad = async (cookies, url) => {
-	console.log(url);
+export const load: LayoutServerLoad = async () => {
 	return {
-		nodeEnv: NODE_ENV,
-		url: url
+		nodeEnv: NODE_ENV
 	};
 };
