248efe7376469edd0cb8bbc4bf8dffa38eb09a65
Nate Houk <nate@epsilonrecords.com>
2024-07-11 00:28:11 +0200

    Refactor

--- src/routes/(websites)/a/[slug]/about/+page.svelte
+++ src/routes/(websites)/a/[slug]/about/+page.svelte
@@ -12,31 +12,29 @@
 	<title>{stageName} — About</title>
 </svelte:head>
 
-<div class="container">
-	<main>
-		<h1>About</h1>
-		<p>
-			{biography}
-		</p>
-		<h2>Skills</h2>
-		{#each Object.entries(skills) as [section, technologies]}
-			<ul>
-				<li>
-					<h4>
-						{section}:
-					</h4>
-					<div class="list">
-						{#each technologies as technology}
-							<div>
-								{technology}
-							</div>
-						{/each}
-					</div>
-				</li>
-			</ul>
-		{/each}
-	</main>
-</div>
+<main>
+	<h1>About</h1>
+	<p>
+		{biography}
+	</p>
+	<h2>Skills</h2>
+	{#each Object.entries(skills) as [section, technologies]}
+		<ul>
+			<li>
+				<h4>
+					{section}:
+				</h4>
+				<div class="list">
+					{#each technologies as technology}
+						<div>
+							{technology}
+						</div>
+					{/each}
+				</div>
+			</li>
+		</ul>
+	{/each}
+</main>
 
 <style>
 	.container {
