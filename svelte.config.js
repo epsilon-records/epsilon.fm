1538c8666d5efe04be576d9215c9d45c3fab4577
Nate Houk <nate@epsilonrecords.com>
2024-07-12 18:05:02 +0200

    Refactor

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		csrf: {
-			checkOrigin: false
+			checkOrigin: true
 		},
 		alias: {
 			'@/*': './src/lib/*'
