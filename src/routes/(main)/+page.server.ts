d942a29deadd9fc59a0314fa2c76378770617b5c
Nate Houk <nate@epsilonrecords.com>
2024-07-12 17:05:59 +0200

    Refactor

--- src/routes/(main)/+page.server.ts
+++ src/routes/(main)/+page.server.ts
@@ -2,13 +2,9 @@ import type { PageServerLoad } from './$types.js';
 import { clerkClient } from '@clerk/clerk-sdk-node';
 
 export const load: PageServerLoad = async ({ locals }) => {
-	let slug = '';
-	if (locals.session.claims !== undefined) {
-		const organizationId = locals.session.claims.org_id ?? null;
-		const response = await clerkClient.organizations.getOrganization({ organizationId });
-		slug = response.slug ?? '';
-	}
+	const organizationId = locals.session.claims.org_id ?? null;
+	const response = await clerkClient.organizations.getOrganization({ organizationId });
 	return {
-		slug: slug
+		slug: response.slug ?? ''
 	};
 };
