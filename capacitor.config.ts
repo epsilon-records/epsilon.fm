1d311ffd66a1352fc09f1c7addf5c6b4399237b2
Nate Houk <nate@epsilonrecords.com>
2024-07-13 09:42:40 +0200

    Add Tauri 2.0

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+	appId: 'fm.epsilon.app',
+	appName: 'Epsilon',
+	webDir: 'build'
+};
+
+export default config;
