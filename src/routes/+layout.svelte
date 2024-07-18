94c1ab98c0d70d7c9d19501312a108ebf51c3dd7
Nate Houk <nate@epsilonrecords.com>
2024-07-18 11:10:28 +0200

    chore: 🔧 Refactor

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,7 @@
-<script lang="ts">
-	import { PUBLIC_METICULOUS_RECORDING_TOKEN, PUBLIC_WEGLOT_API_KEY } from '$env/static/public';
-</script>
-
 <svelte:head>
 	{#if import.meta.env.PUBLIC_METICULOUS_RECORDING_TOKEN}
 		<script
-			data-project-id={PUBLIC_METICULOUS_RECORDING_TOKEN}
+			data-project-id={import.meta.env.PUBLIC_METICULOUS_RECORDING_TOKEN}
 			data-is-production-environment={import.meta.env.MODE === 'production'}
 			src="https://snippet.meticulous.ai/v1/meticulous.js"
 		></script>
@@ -14,7 +10,7 @@
 		<script type="text/javascript" src="https://cdn.weglot.com/weglot.min.js"></script>
 		<script>
 			Weglot.initialize({
-				api_key: PUBLIC_WEGLOT_API_KEY
+				api_key: import.meta.env.PUBLIC_WEGLOT_API_KEY
 			});
 		</script>
 	{/if}
