98b779feef46de1ea2328ba9c3a1fc587e9f396a
Nate Houk <nate@epsilonrecords.com>
2024-07-18 10:58:18 +0200

    chore: 🔧 Refactor

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 <svelte:head>
 	<script
 		data-project-id={METICULOUS_RECORDING_TOKEN}
-		data-is-production-environment={import.meta.env.PROD}
+		data-is-production-environment={import.meta.env.MODE === 'production'}
 		src="https://snippet.meticulous.ai/v1/meticulous.js"
 	></script>
 	<script type="text/javascript" src="https://cdn.weglot.com/weglot.min.js"></script>
