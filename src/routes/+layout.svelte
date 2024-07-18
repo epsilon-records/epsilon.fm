2c1b51bb8950a76cdd512779e497d05a1c30331d
Nate Houk <nate@epsilonrecords.com>
2024-07-18 11:06:17 +0200

    chore: 🔧 Refactor

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,21 @@
 <script lang="ts">
-	import { METICULOUS_RECORDING_TOKEN, WEGLOT_API_KEY } from '$env/static/private';
+	import { PUBLIC_METICULOUS_RECORDING_TOKEN, PUBLIC_WEGLOT_API_KEY } from '$env/static/public';
 </script>
 
 <svelte:head>
-	<script
-		data-project-id={METICULOUS_RECORDING_TOKEN}
-		data-is-production-environment={import.meta.env.MODE === 'production'}
-		src="https://snippet.meticulous.ai/v1/meticulous.js"
-	></script>
-	<script type="text/javascript" src="https://cdn.weglot.com/weglot.min.js"></script>
-	<script>
-		Weglot.initialize({
-			api_key: WEGLOT_API_KEY
-		});
-	</script>
+	{#if import.meta.env.PUBLIC_METICULOUS_RECORDING_TOKEN}
+		<script
+			data-project-id={PUBLIC_METICULOUS_RECORDING_TOKEN}
+			data-is-production-environment={import.meta.env.MODE === 'production'}
+			src="https://snippet.meticulous.ai/v1/meticulous.js"
+		></script>
+	{/if}
+	{#if import.meta.env.PUBLIC_WEGLOT_API_KEY}
+		<script type="text/javascript" src="https://cdn.weglot.com/weglot.min.js"></script>
+		<script>
+			Weglot.initialize({
+				api_key: PUBLIC_WEGLOT_API_KEY
+			});
+		</script>
+	{/if}
 </svelte:head>
