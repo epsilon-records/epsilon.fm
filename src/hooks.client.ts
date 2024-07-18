751a8b100def709397d39aa082e731faaa689c71
Nate Houk <nate@epsilonrecords.com>
2024-07-18 10:03:15 +0200

    chore: 🔧 Refactor

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -2,11 +2,11 @@ import logger from '$lib/logger/logger';
 import * as Sentry from '@sentry/sveltekit';
 import type { HandleClientError } from '@sveltejs/kit';
 import { initializeClerkClient } from 'clerk-sveltekit/client';
-import { SENTRY_DSN } from '$env/static/private';
+import { PUBLIC_SENTRY_DSN } from '$env/static/public';
 import { PUBLIC_CLERK_PUBLISHABLE_KEY } from '$env/static/public';
 
 Sentry.init({
-	dsn: SENTRY_DSN,
+	dsn: PUBLIC_SENTRY_DSN,
 	tracesSampleRate: 1,
 	replaysSessionSampleRate: 0.1,
 	replaysOnErrorSampleRate: 1,
