a325e70fbb9c50f6cb91dd3e8477e8d1dd7c1784
Nate Houk <nate@epsilonrecords.com>
2024-07-18 10:15:20 +0200

    chore: 🔧 Refactor

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,7 @@
 import * as Sentry from '@sentry/sveltekit';
 import { sequence } from '@sveltejs/kit/hooks';
 import { handleClerk } from 'clerk-sveltekit/server';
-import { SENTRY_DSN } from '$env/dynamic/public';
+import { PUBLIC_SENTRY_DSN } from '$env/static/public';
 import { CLERK_SECRET_KEY } from '$env/static/private';
 import { redirect } from '@sveltejs/kit';
 import type { Handle } from '@sveltejs/kit';
@@ -11,7 +11,7 @@ type SubdomainRoutes = {
 };
 
 Sentry.init({
-	dsn: SENTRY_DSN,
+	dsn: PUBLIC_SENTRY_DSN,
 	tracesSampleRate: 1
 });
 
