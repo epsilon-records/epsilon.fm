b5188373bc43a0ba5c479495797f3febbf0f88d1
Nate Houk <nate@epsilonrecords.com>
2024-07-18 23:05:26 +0200

    chore: 🔧 Refactor

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,14 +7,14 @@
 </script>
 
 <svelte:head>
-	{#if PUBLIC_METICULOUS_RECORDING_TOKEN !== 'undefined'}
+	{#if PUBLIC_METICULOUS_RECORDING_TOKEN}
 		<script
 			data-project-id={PUBLIC_METICULOUS_RECORDING_TOKEN}
 			data-is-production-environment={PUBLIC_MODE === 'production'}
 			src="https://snippet.meticulous.ai/v1/meticulous.js"
 		></script>
 	{/if}
-	{#if PUBLIC_WEGLOT_API_KEY !== 'undefined'}
+	{#if PUBLIC_WEGLOT_API_KEY}
 		<script type="text/javascript" src="https://cdn.weglot.com/weglot.min.js"></script>
 		<script>
 			Weglot.initialize({
