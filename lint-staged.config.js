7b2f59c9fcf813c1192b8f6f08fad5f2ad5bb382
Nate Houk <nate@epsilonrecords.com>
2024-07-19 08:20:53 +0200

    chore: 🔧 Refactor

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 export default {
-	'**/*.{ts,js,svelte,css,html}': ['prettier --write --list-different', 'eslint --fix'],
+	'**/*.{ts,js,svelte,css,html,json}': ['prettier --write --list-different', 'eslint --fix'],
 	'backend/**/*.py': [
 		'poetry --directory ./backend run ruff check --fix',
 		'poetry --directory ./backend run ruff format'
