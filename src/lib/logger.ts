b3ad07c9efa15b4ae4a4e4802783a1568a531a72
Nate Houk <nate@epsilonrecords.com>
2024-07-19 10:58:17 +0200

    chore: 🔧 Refactor

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,30 +1,5 @@
 import pino from 'pino';
-import { logflarePinoVercel } from 'pino-logflare';
-import { PUBLIC_LOGFLARE_API_KEY, PUBLIC_LOGFLARE_SOURCE_TOKEN } from '$env/static/public';
 
-// Browser logs are draining to https://logflare.app/sources/32475
-// Server logs are draining to https://logflare.app/sources/32465
-
-const { stream, send } = logflarePinoVercel({
-	apiKey: PUBLIC_LOGFLARE_API_KEY,
-	sourceToken: PUBLIC_LOGFLARE_SOURCE_TOKEN
-});
-
-const logger = pino(
-	{
-		browser: {
-			transmit: {
-				level: 'info',
-				send: send
-			}
-		},
-		level: 'debug',
-		base: {
-			env: import.meta.env.VERCEL_ENV,
-			revision: import.meta.env.VERCEL_GIT_COMMIT_SHA
-		}
-	},
-	stream
-);
+const logger = pino();
 
 export default logger;
