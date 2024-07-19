4ac132f7bc952f8fd09d83610b88ea9219c9888f
Nate Houk <nate@epsilonrecords.com>
2024-07-19 11:08:11 +0200

    chore: 🔧 Refactor

--- src/routes/(main)/+layout.server.ts
+++ src/routes/(main)/+layout.server.ts
@@ -1,9 +1,8 @@
 import type { LayoutServerLoad } from './$types';
-import { VERCEL_GIT_COMMIT_SHA } from '$env/static/private';
 
 export const load: LayoutServerLoad = async () => {
 	return {
-		env: import.meta.env.VERCEL_MODE,
-		gitCommitSha: VERCEL_GIT_COMMIT_SHA
+		env: import.meta.env.MODE ?? 'development',
+		gitCommitSha: import.meta.env.VERCEL_GIT_COMMIT_SHA ?? 'unknown'
 	};
 };
