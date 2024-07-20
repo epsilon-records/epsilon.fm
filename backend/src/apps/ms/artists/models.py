19211d1d52e4f3b72600591ab30ae369f9d98615
Nate Houk <nate@epsilonrecords.com>
2024-07-20 20:00:38 +0200

    feat: ✨ Refactor API

--- backend/src/apps/ms/artists/models.py
+++ backend/src/apps/ms/artists/models.py
@@ -16,66 +16,57 @@ class ArtistContentBase(Base):
         max_length=1000,
         description="Artist Biography",
     )
-    spotify_artist_link: Optional[str] = Field(
+    spotify_artist_link: str = Field(
         default="",
         max_length=255,
-        nullable=False,
         description="Artist Spotify URI",
         regex=r"^(https?:\/\/)?(www\.)?open\.spotify\.com\/artist\/[^\s]+$",
     )
-    youtube_channel_link: Optional[str] = Field(
+    youtube_channel_link: str = Field(
         default="",
         max_length=255,
-        nullable=False,
         description="Artist YouTube URL",
         regex=r"^(https?:\/\/)?(www\.)?youtube\.com\/(channel\/|user\/|c\/)?[^\s]+$",
     )
-    instagram_profile_link: Optional[str] = Field(
+    instagram_profile_link: str = Field(
         default="",
         max_length=255,
-        nullable=False,
         description="Artist Instagram URL",
         regex=r"^(https?:\/\/)?(www\.)?instagram\.com\/[^\s]+$",
     )
-    facebook_profile_link: Optional[str] = Field(
+    facebook_profile_link: str = Field(
         default="",
         max_length=255,
-        nullable=False,
         description="Artist Facebook URL",
         regex=r"^(https?:\/\/)?(www\.)?facebook\.com\/[^\s]+$",
     )
-    x_profile_link: Optional[str] = Field(
+    x_profile_link: str = Field(
         default="",
         max_length=255,
-        nullable=False,
         description="Artist X URL",
         regex=r"^(https?:\/\/)?(www\.)?twitter\.com\/[^\s]+$",
     )
-    tiktok_profile_link: Optional[str] = Field(
+    tiktok_profile_link: str = Field(
         default="",
         max_length=255,
-        nullable=False,
         description="Artist TikTok URL",
         regex=r"^(https?:\/\/)?(www\.)?tiktok\.com\/@[^\s]+$",
     )
-    soundcloud_profile_link: Optional[str] = Field(
+    soundcloud_profile_link: str = Field(
         default="",
         max_length=255,
-        nullable=False,
         description="Artist SoundCloud URL",
         regex=r"^(https?:\/\/)?(www\.)?soundcloud\.com\/[^\s]+$",
     )
-    songkick_profile_link: Optional[str] = Field(
+    songkick_profile_link: str = Field(
         default="",
         max_length=255,
-        nullable=False,
         description="Artist SongKick URL",
         regex=r"^(https?:\/\/)?(www\.)?songkick\.com\/artists\/[^\s]+$",
     )
-    bandsintown_profile_link: Optional[str] = Field(
+    bandsintown_profile_link: str = Field(
         default="",
         max_length=255,
-        nullable=False,
         description="Artist BandsInTown URL",
         regex=r"^(https?:\/\/)?(www\.)?bandsintown\.com\/a\/[^\s]+$",
     )
