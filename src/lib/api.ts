0b8039999eacb15cfff6f3fb63bb0cf8a1805dcf
Nate Houk <nate@epsilonrecords.com>
2024-07-20 11:18:47 +0200

    chore: 🔧 Add TanStack Query.

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,10 +1,11 @@
 import { artistSchema } from '../routes/(main)/profile/schema';
+import { API_URL, API_VERSION } from '$env/static/private';
 import { z } from 'zod';
 
 export const api = (customFetch = fetch) => ({
 	getArtists: async (): Promise<z.infer<typeof artistSchema>[]> => {
 		try {
-			const response = await customFetch('https://jsonplaceholder.typicode.com/posts');
+			const response = await customFetch(`${API_URL}/${API_VERSION}/artist`);
 			const data = await response.json();
 			const parsedData = artistSchema.array().parse(data);
 			return parsedData;
@@ -13,15 +14,15 @@ export const api = (customFetch = fetch) => ({
 			throw new Error('Failed to fetch artists');
 		}
 	},
-	getArtistById: async (id: number): Promise<z.infer<typeof artistSchema>> => {
+	getArtist: async (slug: string): Promise<z.infer<typeof artistSchema>> => {
 		try {
-			const response = await customFetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+			const response = await customFetch(`${API_URL}/${API_VERSION}/artist/${slug}`);
 			const data = await response.json();
 			const parsedData = artistSchema.parse(data);
 			return parsedData;
 		} catch (error) {
-			console.error(`Error fetching artist with id ${id}:`, error);
-			throw new Error(`Failed to fetch artist with id ${id}`);
+			console.error(`Error fetching artist with slug ${slug}:`, error);
+			throw new Error(`Failed to fetch artist with slug ${slug}`);
 		}
 	}
 });
--- src/routes/(website)/a/[slug]/+page.ts
+++ src/routes/(website)/a/[slug]/+page.ts
@@ -1,11 +1,14 @@
 import { api } from '$lib/api';
+import { clerkClient } from '@clerk/clerk-sdk-node';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ parent, fetch }) => {
+export const load: PageLoad = async ({ params, parent, fetch }) => {
+	const response = await clerkClient.organizations.getOrganization({
+		slug: params.slug
+	});
 	const { queryClient } = await parent();
-
 	await queryClient.prefetchQuery({
 		queryKey: ['artists'],
-		queryFn: () => api(fetch).getArtists()
+		queryFn: () => api(fetch).getArtist(response.id)
 	});
 };
