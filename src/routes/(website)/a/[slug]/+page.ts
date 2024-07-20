cc8e3568aa16062908974e96260f6585a03d0d62
Nate Houk <nate@epsilonrecords.com>
2024-07-20 12:47:43 +0200

    chore: 🔧 Add TanStack Query.

--- src/routes/(website)/a/[slug]/+page.ts
+++ src/routes/(website)/a/[slug]/+page.ts
@@ -1,14 +1,10 @@
-import { api } from '$lib/api';
-import { clerkClient } from '@clerk/clerk-sdk-node';
 import type { PageLoad } from './$types';
+import { api } from '$lib/api';
 
-export const load: PageLoad = async ({ params, parent, fetch }) => {
-	const response = await clerkClient.organizations.getOrganization({
-		slug: params.slug
-	});
-	const { queryClient } = await parent();
+export const load = (async ({ parent }) => {
+	const { queryClient, orgId } = await parent();
 	await queryClient.prefetchQuery({
-		queryKey: ['artists'],
-		queryFn: () => api(fetch).getArtist(response.id)
+		queryKey: ['artist'],
+		queryFn: async () => api().getArtist(orgId)
 	});
-};
+}) satisfies PageLoad;
