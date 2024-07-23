2e8f036524ba840baaa5f17c6c9e8e5ec688a02a
Nate Houk <nate@epsilonrecords.com>
2024-07-23 14:06:38 +0200

    chore: 🔧 Refactor

--- src/routes/(main)/settings/+layout.svelte
+++ src/routes/(main)/settings/+layout.svelte
@@ -9,19 +9,19 @@
 		},
 		{
 			title: 'Account',
-			href: '/forms/account'
+			href: '/settings/account'
 		},
 		{
 			title: 'Appearance',
-			href: '/forms/appearance'
+			href: '/settings/appearance'
 		},
 		{
 			title: 'Notifications',
-			href: '/forms/notifications'
+			href: '/settings/notifications'
 		},
 		{
 			title: 'Display',
-			href: '/forms/display'
+			href: '/settings/display'
 		}
 	];
 </script>
