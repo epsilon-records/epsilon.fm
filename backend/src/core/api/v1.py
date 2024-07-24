644e994d0002514829e981b88838a8003d8ff7c7
Nate Houk <nate@epsilonrecords.com>
2024-07-24 15:26:43 +0200

    chore: 🔧 Refactor

--- backend/src/core/api/v1.py
+++ backend/src/core/api/v1.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from fastcrud import crud_router
 
 # Local Dependencies
+from src.core.db.session import async_get_db
 from src.apps.auth.routers.v1 import router as auth_router
 from src.apps.system.tiers.routers.v1 import router as tiers_router
 from src.apps.system.rate_limits.routers.v1 import router as rate_limits_router
@@ -11,9 +12,8 @@ from src.apps.admin.tasks.routers.v1 import router as tasks_router
 from src.apps.blog.posts.routers.v1 import router as posts_router
 from src.apps.ms.artists.routers.v1 import router as artists_router
 from src.apps.system.utils.routers.v1 import router as utils_router
-from src.core.db.session import async_get_db
-from src.core.ms.tracks.models import Track
-from src.core.ms.tracks.schemas import Track as TrackSchema
+from src.apps.ms.tracks.models import Track
+from src.apps.ms.tracks.schemas import Track as TrackSchema
 
 # Create an APIRouter instance for versioning and prefixing routes
 api_v1_router = APIRouter(prefix="/v1")
