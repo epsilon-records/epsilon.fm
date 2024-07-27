dc6fff0970235967eb70baedb984ea981d0cb05a
Nate Houk <nate@epsilonrecords.com>
2024-07-27 15:16:46 +0200

    chore: 🔧 Refactor

--- src/routes/(main)/pricing/+page.svelte
+++ src/routes/(main)/pricing/+page.svelte
@@ -1,23 +1,9 @@
 <script lang="ts">
-	import * as Card from '$lib/components/ui/card';
-	import * as Accordion from '$lib/components/ui/accordion';
 </script>
 
-<Card.Root class="m-4">
-	<Card.Header>
-		<Card.Title>FAQ</Card.Title>
-		<Card.Description>Frequently Asked Questions ❓</Card.Description>
-	</Card.Header>
-	<Card.Content>
-		<Accordion.Root class="w-full self-center sm:max-w-[100%]">
-			<Accordion.Item value="item-1">
-				<Accordion.Trigger>Is it free?</Accordion.Trigger>
-				<Accordion.Content>No.</Accordion.Content>
-			</Accordion.Item>
-			<Accordion.Item value="item-2">
-				<Accordion.Trigger>Do you deliver to Spotify?</Accordion.Trigger>
-				<Accordion.Content>Yes.</Accordion.Content>
-			</Accordion.Item>
-		</Accordion.Root>
-	</Card.Content>
-</Card.Root>
+<svelte:head><script async src="https://js.stripe.com/v3/pricing-table.js"></script></svelte:head>
+<stripe-pricing-table
+	pricing-table-id="prctbl_1PhARlGR0puR9CIOLNjVzLOX"
+	publishable-key="pk_live_ETuT2S2KuFBA7BWjHJmYmXpJ"
+>
+</stripe-pricing-table>
