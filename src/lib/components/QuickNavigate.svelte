de8dbe5a2c59327aaee03e104fe433f1245801f6
Nate Houk <nate@epsilonrecords.com>
2024-07-30 14:52:26 +0200

    feat: ✨ Refactor UI

--- src/lib/components/QuickNavigate.svelte
+++ src/lib/components/QuickNavigate.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
 	import * as Menubar from '$lib/components/ui/menubar/index.js';
 	import { page } from '$app/stores';
+	import { goto } from '$app/navigation';
+
+	const items = ['dashboard', 'music', 'orders', 'products', 'customers', 'analytics'];
+
+	function handleKeydown(event: KeyboardEvent) {
+		const key = parseInt(event.key);
+		if (!isNaN(key) && key > 0 && key <= items.length) {
+			goto(`/${items[key - 1]}`);
+		}
+	}
 </script>
 
+<svelte:window on:keydown={handleKeydown} />
+
 <Menubar.RadioGroup value={$page.url.pathname.split('/')[1] || 'dashboard'}>
-	{#each ['dashboard', 'music', 'orders', 'products', 'customers', 'analytics'] as item, index}
+	{#each items as item, index}
 		<a href="/{item}">
 			<Menubar.RadioItem disabled={$page.url.pathname.startsWith(`/${item}`)} value={item}>
 				{item.charAt(0).toUpperCase() + item.slice(1)}
