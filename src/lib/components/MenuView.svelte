3078a2a0c7f2947b749ea74b1e4b515d8b241918
Nate Houk <nate@epsilonrecords.com>
2024-07-30 15:36:54 +0200

    feat: ✨ Add key bindings.

--- src/lib/components/MenuView.svelte
+++ src/lib/components/MenuView.svelte
@@ -3,7 +3,40 @@
 	import QuickNavigate from './QuickNavigate.svelte';
 	import ToggleFullScreen from './ToggleFullScreen.svelte';
 	import * as Menubar from '$lib/components/ui/menubar/index';
+	import { goto } from '$app/navigation';
+
+	const items = ['dashboard', 'music', 'orders', 'products', 'customers', 'analytics'];
+
+	let isFullScreen = false;
+
+	function toggleFullScreen() {
+		if (!document.fullscreenElement) {
+			document.documentElement.requestFullscreen();
+			isFullScreen = true;
+		} else {
+			if (document.exitFullscreen) {
+				document.exitFullscreen();
+				isFullScreen = false;
+			}
+		}
+	}
+
+	function handleKeydown(event: KeyboardEvent) {
+		if (!event.metaKey) return;
+		if (event.key === 'f') {
+			event.preventDefault();
+			toggleFullScreen();
+		} else {
+			const key = parseInt(event.key);
+			if (!isNaN(key) && key > 0 && key <= items.length) {
+				event.preventDefault();
+				goto(`/${items[key - 1]}`);
+			}
+		}
+	}
 </script>
+
+<svelte:window on:keydown={handleKeydown} />
 
 <Menubar.Menu>
 	<Menubar.Trigger>View</Menubar.Trigger>
--- src/lib/components/QuickNavigate.svelte
+++ src/lib/components/QuickNavigate.svelte
@@ -1,19 +1,8 @@
 <script lang="ts">
 	import * as Menubar from '$lib/components/ui/menubar/index.js';
 	import { page } from '$app/stores';
-	import { goto } from '$app/navigation';
 
 	const items = ['dashboard', 'music', 'orders', 'products', 'customers', 'analytics'];
-
-	function handleKeydown(event: KeyboardEvent) {
-		console.log(event.key);
-		const key = parseInt(event.key);
-
-		if (!isNaN(key) && key > 0 && key <= items.length) {
-			goto(`/${items[key - 1]}`);
-			console.log(items[key - 1]);
-		}
-	}
 </script>
 
 <Menubar.RadioGroup value={$page.url.pathname.split('/')[1] || 'dashboard'}>
@@ -21,7 +10,7 @@
 		<a href="/{item}">
 			<Menubar.RadioItem disabled={$page.url.pathname.startsWith(`/${item}`)} value={item}>
 				{item.charAt(0).toUpperCase() + item.slice(1)}
-				<Menubar.Shortcut>{index + 1}</Menubar.Shortcut>
+				<Menubar.Shortcut>⌘{index + 1}</Menubar.Shortcut>
 			</Menubar.RadioItem>
 		</a>
 	{/each}
