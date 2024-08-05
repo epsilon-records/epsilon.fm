cbfe727c5d27f64943c4d587acf67bb20c13f732
Nate Houk <nate@epsilonrecords.com>
2024-08-05 19:34:32 +0200

    feat: ✨ Add Cron, API clients, and refactor UI.

--- src/routes/api/chat/+page.server.ts
+++ src/routes/api/chat/+page.server.ts
@@ -3,7 +3,6 @@ import { getChats } from '$lib/chat';
 import _ from 'lodash';
 
 export const load: PageServerLoad = async ({ locals }) => {
-	// Use type assertion to access 'session'
 	const session = _.get(locals, 'session') as { userId?: string } | undefined;
 	const userId = _.get(session, 'userId');
 	if (userId) {
