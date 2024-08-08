403d1c13f7da89e0b1bb1048d0ff6bc7ad4f445d
Nate Houk <nate@epsilonrecords.com>
2024-08-08 13:51:53 +0200

    feat: ✨ Analytics

--- backend/src/core/api/v1.py
+++ backend/src/core/api/v1.py
@@ -5,13 +5,8 @@ from fastcrud import crud_router
 # Local Dependencies
 from src.core.db.session import async_get_db
 from src.apps.auth.routers.v1 import router as auth_router
-from src.apps.system.tiers.routers.v1 import router as tiers_router
-from src.apps.system.rate_limits.routers.v1 import router as rate_limits_router
-from src.apps.admin.users.routers.v1 import router as users_router
-from src.apps.admin.tasks.routers.v1 import router as tasks_router
-from src.apps.blog.posts.routers.v1 import router as posts_router
-from src.apps.ms.artists.routers.v1 import router as artists_router
-from src.apps.system.utils.routers.v1 import router as utils_router
+from src.apps.ms.artists.models import Artist
+from src.apps.ms.artists.schemas import ArtistCreateInternal, ArtistUpdateInternal
 from src.apps.ms.tracks.models import Track
 from src.apps.ms.tracks.schemas import TrackCreateInternal, TrackUpdateInternal
 from src.apps.ms.genres.models import Genre
@@ -24,13 +19,23 @@ api_v1_router = APIRouter(prefix="/v1")
 
 # Include routers for different modules and functionalities under the v1 API version
 api_v1_router.include_router(auth_router)
-api_v1_router.include_router(tiers_router)
-api_v1_router.include_router(rate_limits_router)
-api_v1_router.include_router(users_router)
-api_v1_router.include_router(tasks_router)
-api_v1_router.include_router(posts_router)
-api_v1_router.include_router(artists_router)
-api_v1_router.include_router(utils_router)
+# api_v1_router.include_router(tiers_router)
+# api_v1_router.include_router(rate_limits_router)
+# api_v1_router.include_router(users_router)
+# api_v1_router.include_router(tasks_router)
+# api_v1_router.include_router(posts_router)
+# api_v1_router.include_router(utils_router)
+api_v1_router.include_router(
+    crud_router(
+        session=async_get_db,
+        model=Artist,
+        create_schema=ArtistCreateInternal,
+        update_schema=ArtistUpdateInternal,
+        path="/artists",
+        tags=["Artists"],
+        included_methods=["create", "read", "read_paginated", "update", "delete"],
+    )
+)
 api_v1_router.include_router(
     crud_router(
         session=async_get_db,
