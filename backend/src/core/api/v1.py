ad9af3b8e516eae993d13660aac89a4ef3931aa7
Nate Houk <nate@epsilonrecords.com>
2024-08-09 08:03:28 +0200

    feat: ✨ Analytics

--- backend/src/core/api/v1.py
+++ backend/src/core/api/v1.py
@@ -28,6 +28,15 @@ api_v1_router.include_router(
         path="/artists",
         tags=["Artists"],
         # included_methods=["create", "read", "read_multiple", "update", "delete"],
+        endpoint_names={
+            "create": "",
+            "read": "",
+            "update": "",
+            "delete": "",
+            "db_delete": "",
+            "read_multi": "",
+            "read_paginated": "get_paginated",
+        },
     )
 )
 api_v1_router.include_router(
@@ -39,6 +48,15 @@ api_v1_router.include_router(
         path="/tracks",
         tags=["Tracks"],
         # included_methods=["create", "read", "read_multiple", "update", "delete"],
+        endpoint_names={
+            "create": "",
+            "read": "",
+            "update": "",
+            "delete": "",
+            "db_delete": "",
+            "read_multi": "",
+            "read_paginated": "",
+        },
     )
 )
 api_v1_router.include_router(
